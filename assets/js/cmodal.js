dc6b9fbd2a178eeace97412763e23856747b6d65
SABAS <sabaseduofficial@gmail.com>
2024-09-04 12:14:29 +0600

    Delete assets/js/cmodal.js

--- assets/js/cmodal.js
+++
@@ -1,9 +0,0 @@
-const contact_btn = document.querySelector('.contact-btn');
-const close_btn = document.querySelector('.close-btn');
-const contact_container = document.querySelector('.contact-container');
-contact_btn.addEventListener('click', () => {
-    contact_container.classList.toggle('visible')
-});
-close_btn.addEventListener('click', () => {
-    contact_container.classList.remove('visible')
-});
